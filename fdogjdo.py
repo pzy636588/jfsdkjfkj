98c6567c2aa2b45b664805b7cfabb1ffb0690457
pengziyu_pro <pengziyu888@163.com>
2020-09-17 22:18:01 +0800

    练习代码20200917

--- fdogjdo.py
+++ fdogjdo.py
@@ -7,6 +7,40 @@
 
 
 
-a=34
-b=45
-print("a-c")
+skill='跑'
+if skill =='跑':
+    print("进入名牌大学")
+else:
+    print('回家务农')
+
+print('''
+       关关雎鸠
+       在河之洲
+       窈窕淑女
+       君子好逑
+       ''')
+
+total=100
+minus=60
+final=total-minus
+print('权责黄盖'+str(total)+'军棍')
+print('黄盖昏迷,众将士求情')
+print('算了,减'+str(minus)+'军棍,权责黄盖'+str(final)+'军棍')
+
+te1='我'
+te2='爱'
+te3='你'
+print(te1+te2+te3)
+
+#26岁，隆中对策
+#27岁，初出茅庐，赤壁智斗
+#36岁，定鼑荆益
+#41岁，先主托孤
+#53岁，病重而逝
+
+brith_year=181
+print(str(207-brith_year)+'岁，隆中对策')
+print(str(208-brith_year)+'岁，初出茅庐，赤壁智斗')
+print(str(217-brith_year)+'岁，定鼑荆益')
+print(str(222-brith_year)+'岁，先主托孤')
+print(str(234-brith_year)+'岁，病重而逝')
