c66913458c3ee5e7ae09700c1736b5d42067b70b
pengziyu_pro <pengziyu888@163.com>
2020-09-25 22:10:51 +0800

    列表的统计和索引

---
+++ 列表统计.py
@@ -0,0 +1,16 @@
+# _*_coding: UTF-8_*_
+# 开发团队: 彭大工程师
+# 开发人员: penghong
+# 开发时间: 2020/9/25 21:36
+# 文件名称: 列表统计
+# 开发工具: PyCharm
+
+
+player=['莫德里奇','梅西','苏亚雷斯','内马尔','莫德里奇']
+nun=player.count('莫德里奇')
+print(nun)
+
+
+team=['西班牙','阿根廷','法国','德国']
+position=team.index('阿根廷')       #获取元素首次出现的下标
+print(position)
