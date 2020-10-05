d92b08667b2021579e6e2aa840fed605bc9663f2
pengziyu_pro <pengziyu888@163.com>
2020-10-05 21:51:10 +0800

    元组1

---
+++ 元组.py
@@ -0,0 +1,25 @@
+# _*_coding: UTF-8_*_
+# 开发团队: 彭大工程师
+# 开发人员: penghong
+# 开发时间: 2020/10/5 21:41
+# 文件名称: 元组
+# 开发工具: PyCharm
+
+
+# 创建元组只要（）和，
+team='花间','jsfi','sjf'
+ter=23,45,56,78,89
+print(ter)
+print(team)
+
+# 创建空元组
+emptytuple=()
+print(emptytuple)
+
+# 创建数值元组
+tuple(range(100,300,2))
+print(tuple(range(100,300,1)))
+
+# 删除元组
+del ter
+
