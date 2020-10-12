39976682b4c727311456df8ccfd4cf3ba8f6cd89
pengziyu_pro <pengziyu888@163.com>
2020-10-12 21:28:04 +0800

    计算字符串长度

---
+++ 计算字符串的长度.py
@@ -0,0 +1,11 @@
+# _*_coding: UTF-8_*_
+# 开发团队: 彭大工程师
+# 开发人员: penghong
+# 开发时间: 2020/10/12 21:19
+# 文件名称: 计算字符串的长度
+# 开发工具: PyCharm
+
+
+str1='人生苦短，我用python!'               #定义字符串
+length=len(str1.encode())                        #计算字符串的长度
+print(length)
